cd66d9fcb774bd4ae125d60a1047f8a39b6b9de4
Patricio Gonzalez Vivo <patriciogonzalezvivo@gmail.com>
2013-01-30 07:56:16 -0500

    more inteligent palette

---
+++ 03 - PaintBrush/src/SlideDock.cpp
@@ -0,0 +1,9 @@
+//
+//  SlideDock.cpp
+//  03 - PaintBrush
+//
+//  Created by Patricio Gonzalez Vivo on 1/27/13.
+//
+//
+
+#include "SlideDock.h"
---
+++ 03 - PaintBrush/src/SlideDock.h
@@ -0,0 +1,14 @@
+//
+//  SlideDock.h
+//  03 - PaintBrush
+//
+//  Created by Patricio Gonzalez Vivo on 1/27/13.
+//
+//
+
+#ifndef ___3___PaintBrush__SlideDock__
+#define ___3___PaintBrush__SlideDock__
+
+#include <iostream>
+
+#endif /* defined(___3___PaintBrush__SlideDock__) */
